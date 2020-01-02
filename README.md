467e6278dba9f2391f6847be77989a1b1bd861d6
DoctorWhoFR <doctorwhofr@protonmail.com>
2020-01-02 01:48:54 +0100

    Add remove option

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # OnsetPWatcher
 A simple nodejs file that watch for any servers,clients and assets file and add it to your package.json
 
+*Handle, file creation / remove*
+
 **1- You just need to put the folder to any OnsetPackages.**
 
 **2- After that launch 1 time "Install" and run after "Run"**
--- index.js
+++ index.js
@@ -69,11 +69,38 @@ function addToPackageJson(path) {
     }
 }
 
+function removeToPackageJson(path) {
+    if(!path.includes("node_modules\\") && path != "index.js" && path != "package.json" && path != "package-lock.json" && !path.includes("Nouveau document")){
+        path = path.replace(/\\/g, "/")
+        
+        if(path.includes(".html") || path.includes(".js") || path.includes(".css") || path.includes(".png") || path.includes(".jpg") || path.includes(".ttf"))
+        {
+            console.log(jsont.files)
+            if(jsont.files.includes(path)){
+                
+                var yahooOnly = jsont.files.filter(function (entry) {
+                    return entry !== path;
+                });
+                jsont.files = yahooOnly
+                let jsonifyed = JSON.stringify(jsont);
+                console.log(jsonifyed)
+                // Write data in 'Output.txt' . 
+                fs.writeFile('package.json', jsonifyed, (err) => { 
+                    
+                    // In case of a error throw err. 
+                    if (err) throw err; 
+                }) 
+            }
+        }
+
+    }
+}
+
 // Something to use when events are received.
 const log = console.log.bind(console);
 
 watcher
   .on('add', path => addToPackageJson(path))
   .on('change', path => log(`File ${path} has been changed`))
-  .on('unlink', path => log(`File ${path} has been removed`));
+  .on('unlink', path => removeToPackageJson(path));
 
